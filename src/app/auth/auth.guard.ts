7d5b2c4ae5045cddcc2e845ca2fa56f5c24eaf68
almestady <almestady@gmail.com>
2020-10-22 17:21:17 +0300

    Finishing authentication

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanLoad, Route, UrlSegment, Router, CanActivate, CanActivateChild } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, tap } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
 // import { TokenInterceptor } from '../token.interceptor';
 
@@ -12,9 +13,12 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
   canActivate(): Observable<boolean> | Promise<boolean> | boolean {
     console.log("Auth gard started")
-       if (!this.authService.userIsAuthenticated) {
-         this.router.navigateByUrl('auth');
-       }
-       return this.authService.userIsAuthenticated;
+    return this.authService.userIsAuthenticated.pipe(take(1),tap(isAuthenticated => {
+      if (!isAuthenticated) {
+        this.router.navigateByUrl('/auth');
+      }
+
+    }));
+
      }
 }
